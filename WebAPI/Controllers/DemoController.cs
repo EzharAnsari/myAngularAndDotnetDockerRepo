c1fbba4d8f8309bd7c03025c4fb7de53df30cf7b
EzharAnsari <ea199100@gmail.com>
2022-07-24 19:40:49 +0530

    working on kubernetes cluster

---
+++ WebAPI/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("Demo")]
+
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome to docker world!";
+        }
+    }
+}
---
+++ WebAPI/Services/InMemoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Entities;
+
+namespace WebAPI.Services
+{
+    public class InMemoryRepository
+    {
+        private List<Genre> _genres;
+
+        public InMemoryRepository()
+        {
+            _genres = new List<Genre>()
+            {
+                new Genre(){Id=1, Name="Comedy"},
+                new Genre(){Id=2, Name="Action"},
+                new Genre(){Id=3, Name="Mystry"}
+            };
+        }
+
+        public List<Genre> GetAllGenres()
+        {
+            return _genres;
+        }
+    }
+}
